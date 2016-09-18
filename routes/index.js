8cffc0eb98ca9998fa7982d7bf6c11f4f6954c19
Eric Mooney <eric.mooney@gmail.com>
2016-09-18 18:22:59 -0400

    added name and age variables to the page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'My App',
+    age: '33',
+    name: 'Eric'
+  });
+
 });
+
+// router.get('/', function(req, res) {
+//   res.send(200);
+// });
 
 module.exports = router;
